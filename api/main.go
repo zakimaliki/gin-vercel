a8831d38f21ca745f11f9a0c164d69fd2ed133d2
malikizaki94 <malikizakitop@gmail.com>
2023-10-04 02:22:42 +0700

    update

---
+++ api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	app *gin.Engine
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":3000") // listen and serve on 0.0.0.0:8080
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	app.ServeHTTP(w, r)
+}
